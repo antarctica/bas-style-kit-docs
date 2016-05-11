53127d34af1bbad7ad8fa67c3642ce4bc1509089
Felix Fennell <felnne@bas.ac.uk>
2016-05-11 15:25:32 +0100

    Adding logic for different site versions
    This adds two new variables 'branch' and 'x_environment' (environment is reserved by Jekyll).
    
    This lets us automatically include the correct CSS, and give the correct values for templates and examples.

--- site/_plugins/environment_variables.rb
+++ site/_plugins/environment_variables.rb
@@ -11,6 +11,14 @@ module Jekyll
       site.config['revision'] = ENV['REVISION'] || 'unknown'
       site.config['revision'] = site.config['revision'][0..6]
 
+      # If the branch is known, convert to an environment
+      site.config['branch'] = ENV['BRANCH_NAME'] || 'unknown'
+      case site.config['branch']
+      when "master"
+        site.config['x_environment'] = 'production'
+      else
+        site.config['x_environment'] = 'staging'
+      end
     end
 
   end
