0d8b9dc515bbcfa3a24f26eaed8619699cd63578
Felix Fennell <felix@felixfennell.co.uk>
2016-12-18 18:07:03 +0000

    Fixing revision/branch detection plugin
    Fixing logic.

--- site/_plugins/environment_variables.rb
+++ site/_plugins/environment_variables.rb
@@ -8,11 +8,11 @@ module Jekyll
     def generate(site)
 
       # If the revision is known, take the first 7 characters
-      site.config['revision'] = ENV['REVISION'] || ENV['CI_BUILD_REF'] || 'unknown'
+      site.config['revision'] = (ENV['REVISION'] if ENV.has_key?('REVISION')) || (ENV['CI_BUILD_REF'] if ENV.has_key?('CI_BUILD_REF')) || "unknown"
       site.config['revision'] = site.config['revision'][0..6]
 
       # If the branch is known, convert to an environment
-      site.config['branch'] = ENV['BRANCH_NAME'] || ENV['CI_BUILD_REF_SLUG'] || 'unknown'
+      site.config['branch'] = (ENV['BRANCH_NAME'] if ENV.has_key?('BRANCH_NAME')) || (ENV['CI_BUILD_REF_SLUG'] if ENV.has_key?('CI_BUILD_REF_SLUG')) || "unknown"
       case site.config['branch']
       when "master"
         site.config['x_environment'] = 'production'
