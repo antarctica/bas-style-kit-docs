aa6b5db3e83b0550dd6c059aee9b12cd5f556e3d
Felix Fennell <felix@felixfennell.co.uk>
2016-12-18 18:17:25 +0000

    Debugging site revision/branch plugin

--- site/_plugins/environment_variables.rb
+++ site/_plugins/environment_variables.rb
@@ -9,10 +9,14 @@ module Jekyll
 
       # If the revision is known, take the first 7 characters
 
+      puts "Amy"
       puts ENV['CI_BUILD_REF']
 
       site.config['revision'] = (ENV['REVISION'] if ENV.has_key?('REVISION')) || (ENV['CI_BUILD_REF'] if ENV.has_key?('CI_BUILD_REF')) || "unknown"
       site.config['revision'] = site.config['revision'][0..6]
+
+      puts "Becca"
+      site.config['revision']
 
       # If the branch is known, convert to an environment
       site.config['branch'] = (ENV['BRANCH_NAME'] if ENV.has_key?('BRANCH_NAME')) || (ENV['CI_BUILD_REF_SLUG'] if ENV.has_key?('CI_BUILD_REF_SLUG')) || "unknown"
