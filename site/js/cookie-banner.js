380f1b1fab5044d4597a405723b34deed2bd77db
Felix Fennell <felix@felixfennell.co.uk>
2016-12-20 14:59:21 +0000

    Fixing cookie banner script

--- site/js/cookie-banner.js
+++ site/js/cookie-banner.js
@@ -2,10 +2,10 @@ jQuery(function($){
 
   // Look for cookie to hide cookie banner
 
-  var cookkieBanner = Cookies.get('seen_cookie_message');
+  var cookieBanner = Cookies.get('seen_cookie_message');
 
-  if (typeof cookkieBanner !== 'undefined') {
-    if (cookkieBanner == 'yes') {
+  if (typeof cookieBanner !== 'undefined') {
+    if (cookieBanner == 'yes') {
 
       // Since we already have jQuery available we may as well use its convenience methods
       jQuery('#site-cookie-banner').remove();
@@ -18,8 +18,7 @@ jQuery(function($){
 
     // Set a cookie to persist hiding the cookie banner for the next 30 days
     Cookies.set('seen_cookie_message', 'yes', {
-      expires: 30,
-      secure: true
+      expires: 30
     });
   })
 });
