d94d07133e20284f19ec9b9e2edd55e43aaf2307
Felix Fennell <felnne@bas.ac.uk>
2018-06-22 17:51:42 +0100

    Switching to per-domain ACM certificates

--- provisioning/terraform/02-providers.tf
+++ provisioning/terraform/02-providers.tf
@@ -5,13 +5,28 @@
 #
 # The BAS preferred public cloud provider.
 #
-# See https://www.terraform.io/docs/providers/aws/index.html#authentication for how to 
+# See https://www.terraform.io/docs/providers/aws/index.html#authentication for how to
 # configure credentials to use this provider.
 #
 # AWS source: https://aws.amazon.com/
 # Terraform source: https://www.terraform.io/docs/providers/aws/index.html
 provider "aws" {
-  version = "~> 1.0"
+  version = "~> 1.12"
 
   region = "eu-west-1"
+}
+
+# AWS provider - alias
+#
+# This alias is used for resources or data-sources that require the 'us-east-1' region, which is used as a control
+# region by AWS for some services
+#
+# See https://www.terraform.io/docs/providers/aws/index.html#authentication for how to
+# configure credentials to use this provider.
+#
+# AWS source: https://aws.amazon.com/
+# Terraform source: https://www.terraform.io/docs/providers/aws/index.html
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
 }
